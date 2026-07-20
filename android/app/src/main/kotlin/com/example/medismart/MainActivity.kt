c38fa8723e3a9452614947919f58ee68d859a341
uzmatasnim <uzmatasnim06@gmail.com>
2026-07-21 00:48:01 +0600

    Delete android directory

--- android/app/src/main/kotlin/com/example/medismart/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.medismart
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/medismart/medismart/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.medismart.medismart
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
